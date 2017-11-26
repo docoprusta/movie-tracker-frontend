c44f2c7db168a5621d1b2a84037f0521dce86a3f
docoprusta <gyore.soma@gmail.com>
2017-11-26 19:05:00 +0100

    video model added

---
+++ src/app/models/video.ts
@@ -0,0 +1,8 @@
+export class Video {
+    constructor(
+        public closeDate?: string,
+        public title?: string,
+        public lastPosition?: number,
+        public duration?: number
+    ) { }
+}
